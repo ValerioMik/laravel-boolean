04b0a6c640dbc4a8bb631bff4d7aa9bbca45a3f1
ValerioMik <valerio.avallone@hotmail.com>
2022-02-15 12:10:36 +0100

    prova

---
+++ database/migrations/2022_02_15_105639_create_postcards_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostcardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postcards', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('sender');
+            $table->string('addres');
+            $table->string('text');
+            $table->string('image');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postcards');
+    }
+}
